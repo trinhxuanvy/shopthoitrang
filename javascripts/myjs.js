6663e51377819aa0b9f2acd70cdb8711d9f1180b
Trinh Xuan Vy <trinhxuanvy1@gmail.com>
2021-03-09 18:24:05 +0700

    Update 9/3

--- javascripts/myjs.js
+++ javascripts/myjs.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
             checkNav = 0;
         }
     });
+
     var checkSearchNoOf = 0;
     $('.header-icon .btn-search-no-official').click(function (e) {
         e.preventDefault();
@@ -27,8 +28,9 @@ $(document).ready(function () {
             checkSearchNoOf = 0;
         }
     });
+
     var checkUser = 0;
-    $('.header-icon .icon-user-click').click(function (e) { 
+    $('.header-icon .icon-user-click').click(function (e) {
         e.preventDefault();
         if (checkUser == 0) {
             $('.header-icon .menu-drop').addClass('active-menu-drop');
@@ -39,6 +41,7 @@ $(document).ready(function () {
             checkUser = 0;
         }
     });
+
     var swiper = new Swiper('.swiper-banner', {
         pagination: {
             el: '.slide-pagination',
@@ -63,10 +66,10 @@ $(document).ready(function () {
         var posHeader = $('.header-nav').position();
         var topHeader = posHeader.top;
         if ($(this).scrollTop() > topHeader) {
-            $('.header-nav').addClass('header-prod-fixed');
+            $('.header-nav').addClass('header-fixed');
         }
         else {
-            $('.header-nav').removeClass('header-prod-fixed');
+            $('.header-nav').removeClass('header-fixed');
         }
     });
 
@@ -83,24 +86,177 @@ $(document).ready(function () {
             $(this).addClass('active');
         });
     }
+
     $('.owl-carousel').owlCarousel({
-        loop:true,
+        loop: true,
         margin: 20,
-        nav:true,
+        nav: true,
         autoplay: true,
         dots: false,
-        responsive:{
-            0:{
-                items:2,
+        responsive: {
+            0: {
+                items: 2,
                 margin: 10,
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
+    })
+
+    var data = [
+        {
+            id: '1',
+            image: '25_510x600.jpg',
+            name: 'áo sơ mi 1',
+            price: '250,000 VNĐ',
+            sale: '',
+            link: 'test'
+        },
+        {
+            id: '2',
+            image: '25_510x600.jpg',
+            name: 'áo sơ mi 2',
+            price: '200,000 VNĐ',
+            sale: '300,000 VNĐ',
+            link: 'test'
+        },
+        {
+            id: '3',
+            image: '27_510x600.jpg',
+            name: 'áo sơ mi 3',
+            price: '250,000 VNĐ',
+            sale: '',
+            link: 'test'
+        },
+        {
+            id: '4',
+            image: '22_510x600.jpg',
+            name: 'áo sơ mi 4',
+            price: '250,000 VNĐ',
+            sale: '',
+            link: 'test'
+        },
+        {
+            id: '5',
+            image: '20_510x600.jpg',
+            name: 'áo sơ mi 5',
+            price: '250,000 VNĐ',
+            sale: '',
+            link: 'test'
+        },
+    ]
+    var numItems = data.length;
+    var perPage = 2;            
+
+    $(function () {
+        (function (name) {
+            var container = $('#pagination-' + name);
+            var sources = function () {
+                var result = [];
+
+                for (var i = 0; i < numItems; i++) {
+                    result.push(data[i]);
+                }
+                return result;
+            }();
+
+            var options = {
+                dataSource: sources,
+                pageSize: 8,
+                showNavigator: true,
+                callback: function (sources, pagination) {
+                    window.console;
+
+                    var dataHtml = '';
+
+                    $.each(sources, function (index, item) {
+                        dataHtml += `<div class="item wow fadeInUp">
+                        <div class="product-image">
+                            <a href=""><img src="images/${item.image}" alt=""></a>
+                        </div>
+                        <p class="product-name">${item.name}</p>
+                        <p class="product-price">${item.price} <span class="product-price-sale"><del>${item.sale}</del></span></p>
+                        <div class="product-detail">
+                            <a href=""><i class="fa fa-eye" aria-hidden="true"></i></a>
+                            <a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                        </div>
+                        </div>`;
+                    });
+
+                    container.prev().html(dataHtml);
+                }
+            };
+            
+
+            //$.pagination(container, options);
+
+            container.addHook('beforeInit', function () {
+                window.console;
+            });
+            container.pagination(options);
+
+            container.addHook('beforePageOnClick', function () {
+                window.console;
+                //return false
+            });
+        })('product');
+
+
+    })
+
+    
+    $(function(){
+        var filterCheck = 0;
+        $('.all-product .header-item').click(function (e) { 
+            e.preventDefault();
+            var hClass = $('.box-filter').hasClass('box-filter-active');
+            if(hClass){
+                $('.box-filter').removeClass('box-filter-active');
+                $(window).lockscroll(false);
+            }
+            else{
+                $('.box-filter').addClass('box-filter-active');
+                $(window).lockscroll(true);
+            }
+        });
+        $('.all-product .btn-cancel').click(function (e) { 
+            e.preventDefault();
+            var hClass = $('.box-filter').hasClass('box-filter-active');
+            if(hClass){
+                $('.box-filter').removeClass('box-filter-active');
+                $(window).lockscroll(false);
+            }
+        });
+    })
+
+    $(function(){
+        $('.header-nav .fa-search-active').click(function (e) { 
+            e.preventDefault();
+            var hClass = $('.header-nav .box-search-menu').hasClass('box-search-menu-active');
+            if(!hClass){
+                $('.header-nav .box-search-menu').addClass('box-search-menu-active');
+            }
+            else{
+                $('.header-nav .box-search-menu').removeClass('box-search-menu-active');
+            }
+        });
+    })
+
+    $(function(){
+        $('.header-nav .fa-user').click(function (e) { 
+            e.preventDefault();
+            var hClass = $('.header-nav .box-user-login').hasClass('box-user-login-active');
+            if(!hClass){
+                $('.header-nav .box-user-login').addClass('box-user-login-active');
+            }
+            else{
+                $('.header-nav .box-user-login').removeClass('box-user-login-active');
+            }
+        });
     })
 });
 new WOW().init();
